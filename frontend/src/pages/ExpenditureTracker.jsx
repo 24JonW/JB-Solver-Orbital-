76514fd3f08f60d7a36ba4564e968f7e2c29184b
24JonW <your-email@example.com>
2026-06-29 13:21:22 +0800

    Change the base domain to RENDER backend url

--- frontend/src/pages/ExpenditureTracker.jsx
+++ frontend/src/pages/ExpenditureTracker.jsx
@@ -141,6 +141,7 @@ function ExpenditureTracker() {
   const [ ledger, setLedger ] = useState([]); 
   const [currentUser, setCurrentUser] = useState(null); 
 
+  // const API_EXP_URL = 'http://localhost:5001/api/exp';
   const API_EXP_URL = 'https://jb-solver-orbital.onrender.com/api/exp';
   
   const [isModalOpen, setIsModalOpen ] = useState(false); 
